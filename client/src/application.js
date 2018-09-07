e519cb055adf510ee4591d2a84871d5d2cfe29d8
ante <ante.kotarac@gmail.com>
2018-09-07 02:13:20 +0200

    client stub cra plus readme file

---
+++ client/src/application.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './application.css';
+
+class Application extends Component {
+  render() {
+    return (
+      <div className="application">
+        Shop cart stub
+      </div>
+    );
+  }
+}
+
+export default Application;
