dca35a3bb303b6e236beb2b52bf1fbd763fee665
ante <ante.kotarac@gmail.com>
2018-09-07 16:02:51 +0200

    client state - redux initial setup

--- client/src/application.js
+++ client/src/application.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './state/reducers'
 import './application.css';
 
 class Application extends Component {
   render() {
     return (
-      <div className="application">
-        Shop cart stub
-      </div>
+      <Provider store={createStore(reducers)}>
+        <div className="application">
+          Shop cart stub
+        </div>
+      </Provider>
     );
   }
 }
---
+++ client/src/state/reducers/index.js
@@ -0,0 +1,3 @@
+import {combineReducers} from 'redux';
+import productsReducer from './products';
+export default combineReducers({productsReducer});
