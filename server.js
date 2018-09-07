f94d866c1ad585fcdf66df4767663af4d0464411
ante <ante.kotarac@gmail.com>
2018-09-07 18:03:45 +0200

    mongo connection setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+require('./server/startdatabase');
 const app = express();
-
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3001);
+const serverPort = process.env.PORT || 3001;
+
+app.listen(serverPort, () => {
+  console.log(`Server is listening at port ${serverPort}`);
+});
