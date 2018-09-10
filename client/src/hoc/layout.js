9a6485270abc7c27495600cde52f757a5cc906a3
ante <ante.kotarac@gmail.com>
2018-09-10 13:02:06 +0200

    footer with basic styling

--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Footer from '../partialcomponents/footer';
 class Layout extends React.Component {
   render () {
     return  (
       <div>
         {this.props.children}
+        <Footer/>
       </div>
     )
   }
