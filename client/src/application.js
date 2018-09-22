115db22b1cca53c32064a5209b0e614ea019e3ee
ante <ante.kotarac@gmail.com>
2018-09-22 19:36:04 +0200

    router paths decomposition

--- client/src/application.js
+++ client/src/application.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import {getStore} from './state/store';
 import ProductList from './components/productlist';
-import AddProduct from './components/addproduct';
-import DeleteProduct from './components/deleteproduct';
+
 import Cart from './components/cart';
+import Admin from './components/admin';
 import Layout from './hoc/layout';
 import './application.css';
 
@@ -13,12 +14,15 @@ class Application extends Component {
   render() {
     return (
       <Provider store={getStore()}>
-        <Layout>
-          <Cart/>
-          <AddProduct/>
-          <DeleteProduct/>
-          <ProductList/>
-        </Layout>
+          <Router>
+              <Switch>
+                <Layout>
+                  <Route path="/" exact component={ProductList}/>
+                  <Route path="/admin" exact component={Admin}/>
+                  <Route path="/cart" exact component={Cart}/>
+                </Layout>
+              </Switch>
+          </Router>
       </Provider>
     );
   }
--- client/src/partialcomponents/navbar.js
+++ client/src/partialcomponents/navbar.js
@@ -1,33 +1,25 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Navbar extends Component {
   render () {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Home</a>
+        <Link className="navbar-brand" to="/">Home</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/wfilter">Products</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/add">
-                Add product
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/delete">
-                Delete product
-              </a>
-            </li>
           </ul>
           <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                  <a className="nav-link" href="/cart">Your cart<span className="badge badge-secondary">5</span></a>
+                  <Link className="nav-link" to="/admin">Admin</Link>
+              </li>
+              <li className="nav-item">
+                  <Link className="nav-link" to="/cart">Your car
+                    t<span className="badge badge-secondary">5</span></Link>
               </li>
           </ul>
         </div>
