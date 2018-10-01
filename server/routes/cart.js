e50d4adcb37a8c3f3c0c55d365dabbb088123567
ante <ante.kotarac@gmail.com>
2018-10-01 09:33:14 +0200

    product route cleaning

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -9,38 +9,12 @@ cartRouter.post('/', (req,res) => {
   const newCart = new Cart(cart);
   newCart
   .save()
-  .then(res => {
-    res.send('Hello');
-
+  .then(r => {
+    res.send('Cart saved successfully');
   })
   .catch(e => {
-    console.log(e);
-    res.send('Hello');
+    res.send({error: e});
   })
-  // const {error, isValid} = productValidation(product);
-  // if (!isValid) {
-  //   return res.json({error})
-  // }
-  // const {name} = product;
-  // Product.find({name})
-  // .then(foundProduct => {
-  //   if (foundProduct && foundProduct.length) {
-  //     const error = {name: 'Product with that name already exists'};
-  //     return res.json({error})
-  //   }
-  //   new Product(product)
-  //   .save()
-  //   .then(r => {
-  //     res.json(r);
-  //   })
-  //   .catch(e => {
-  //     res.send(e);
-  //   });
-  // })
-  // .catch(e => {
-  //   console.log(e);
-  //   res.send(e);
-  // })
 });
 
 module.exports = cartRouter;
